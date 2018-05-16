117f8b9ba1e13d718decec4f1eb1e1700be0756e
Kazuki Tsubo <tsubok@amazon.com>
2018-05-16 14:12:14 +0000

    v1.01 / Add Filter and Remove feature

--- script.user.js
+++ script.user.js
@@ -2,7 +2,7 @@
 // @name        Bitcointalk thread filter
 // @namespace   kazuki.t
 // @include     https://bitcointalk.org/index.php?*
-// @version     1.00
+// @version     1.01
 // @grant    GM_getValue
 // @grant    GM_setValue
 // @grant    GM_deleteValue
@@ -14,43 +14,88 @@
 
 // Initialize
 const page = location.protocol + "//" + location.host + location.pathname + location.search;
+const allSpan = document.querySelectorAll("span");
 var spanNodes=[], nodesNum=0;
 
+// Add buttons
+document.querySelector("#toppages").innerHTML += " <input value=RECOVER type=button id=recv> <input id=flt type=text><input value=DeleteThem type=button id=alldel>";
+
+
 // Hide threads
-const allSpan = document.querySelectorAll("span");
 for(let span in allSpan){
     if(allSpan[span].id && allSpan[span].id.match(/msg.*/)){
         spanNodes[nodesNum] = allSpan[span].id;
-        const url = allSpan[span].innerHTML.match(/\"https.*\.0\"/)[0].replace(/\"/g, "");
-        console.log(url + ": " + GM_getValue(url));
+        let url = allSpan[span].innerHTML.match(/\"https.*\.0\"/)[0].replace(/\"/g, "");
+        //console.log(url + ": " + GM_getValue(url));
 
         // Hide unnecessary thread
         if(GM_getValue(url) > 0){
-            allSpan[span].parentNode.parentNode.parentNode.style.display="none";
+            specifyParentTrNodeState(allSpan[span], "none");
             console.log("Deleted " + url);
         }
 
         // Create button
         allSpan[span].innerHTML = allSpan[span].innerHTML.replace(/<a /, "<input value=DEL type=button id=btn" + nodesNum + " title=" + url + "> <a ");
         document.getElementById("btn"+nodesNum).onclick=(function(){
-            //alert("test");
-            GM_setValue(this.title, Date.now());
-            this.parentNode.parentNode.parentNode.parentNode.style.display="none";
+            hideThread(this.title, this.parentNode);
         });
         nodesNum++;
     }
 }
 
-// Add recovery button
-let recoveryParent = document.querySelector("#toppages");
-recoveryParent.innerHTML = recoveryParent.innerHTML + " " + "<input value=SHOWALL type=button id=recv>";
-document.getElementById("recv").onclick = function(){
-  const allSpan = document.querySelectorAll("span");
-  for(let span in allSpan){
-    if(allSpan[span].id && allSpan[span].id.match(/msg.*/)){
-        const url = allSpan[span].innerHTML.match(/href=\"https.*\.0\"/)[0].replace(/\"/g, "").replace(/href=/, "");
-        allSpan[span].parentNode.parentNode.parentNode.style.display="";
-        GM_deleteValue(url);
+
+// Add filter&delete feature
+document.getElementById("alldel").onclick = function(){
+    const fltText = document.getElementById("flt").value;
+
+    if(fltText.length < 3){
+        alert("Too short! Filter strings must longer than 3.");
+        return;
     }
-  }
+
+    for(let i=0;i<nodesNum;i++){
+        const btni = document.getElementById("btn"+i);
+        const url = btni.title;
+        if(GM_getValue(url) == null && btni.parentNode.innerHTML.indexOf(fltText) >= 0){
+            hideThread(url, btni.parentNode);
+        }
+    }
 };
+document.getElementById("flt").onchange = function(){
+    const fltText = document.getElementById("flt").value;
+    for(let i=0;i<nodesNum;i++){
+        const btni = document.getElementById("btn"+i);
+        const url = btni.title;
+        if(GM_getValue(url) == null){
+            if(fltText.length < 3 || btni.parentNode.innerHTML.indexOf(fltText) >= 0){
+                specifyParentTrNodeState(btni.parentNode, "");
+            }else{
+                specifyParentTrNodeState(btni.parentNode, "none");
+            }
+        }
+    }
+};
+
+// Add recovery feature(coming back deleted threads in showing page)
+document.getElementById("recv").onclick = function(){
+    for(let i=0;i<nodesNum;i++){
+        const btni = document.getElementById("btn"+i);
+        specifyParentTrNodeState(btni, "");
+        GM_deleteValue(btni.title);
+    }
+};
+
+function specifyParentTrNodeState(child, state){console.log(child);
+  if(child.parentNode.className)
+    child.parentNode.parentNode.style.display=state;
+  else if(child.parentNode.parentNode.className)
+    child.parentNode.parentNode.parentNode.style.display=state;
+  else if(child.parentNode.parentNode.parentNode.className)
+    child.parentNode.parentNode.parentNode.parentNode.style.display=state;
+}
+
+function hideThread(url, node){
+    GM_setValue(url, Date.now());
+    specifyParentTrNodeState(node, "none");
+}
+
